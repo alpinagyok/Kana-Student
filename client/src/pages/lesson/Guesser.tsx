b739eec607ea711aec670c33a49ccbf4a7b463d8
alpinagyok <aleks.kozaev@gmail.com>
2021-06-22 09:10:44 +0300

    Guesser Modal + refactored kana choice

--- client/src/pages/lesson/Guesser.tsx
+++ client/src/pages/lesson/Guesser.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { handleKanaChoice } from '../../service/lesson';
+// import { handleKanaChoice } from '../../service/lesson';
 import { Kana } from '../../store/interfaces';
 
 interface Props {
   randomKanas: Kana[];
   kanaToGuess: Kana;
+  handleKanaChoice: (
+    chosenKana: Kana,
+    kanaToGuess: Kana,
+  ) => void
 }
 
-const Guesser: React.FC<Props> = ({ randomKanas, kanaToGuess }) => (
+const Guesser: React.FC<Props> = ({ randomKanas, kanaToGuess, handleKanaChoice }) => (
   <div>
     {randomKanas.map((kana) => (
       <button key={kana.id} type="button" onClick={() => handleKanaChoice(kana, kanaToGuess)}>{kana.japName}</button>
--- client/src/pages/lesson/index.tsx
+++ client/src/pages/lesson/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { WRITER_LESSON } from '../../store/interfaces';
+import Modal from 'react-modal';
+import { Kana, WRITER_LESSON } from '../../store/interfaces';
 import { shufflePreparedKanas } from '../../store/lesson/reducer';
 import {
   getCurrentKana, getLessonType, getPreparedKanas, getSelectedMaterialsBlock,
@@ -19,12 +20,32 @@ const Lesson: React.FC = () => {
   const lessonType = useSelector(getLessonType);
   const currentKana = useSelector(getCurrentKana);
 
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [correctKana, setCorrectKana] = useState<Kana>();
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+
   useEffect(() => {
     if (!selectedMaterial || !preparedKanas || preparedKanas?.length < 4) history.push('/learn');
     else if (!currentKana) {
       dispatch(shufflePreparedKanas());
     }
+
+    Modal.setAppElement('body');
   }, []);
+
+  const handleKanaChoice = (
+    chosenKana: Kana,
+    kanaToGuess: Kana,
+  ): void => {
+    if (chosenKana.id === kanaToGuess.id) {
+      setIsAnswerCorrect(true);
+    } else {
+      setIsAnswerCorrect(false);
+    }
+    setIsOpen(true);
+    setCorrectKana(currentKana);
+    dispatch(shufflePreparedKanas());
+  };
 
   return (
     <div>
@@ -37,11 +58,31 @@ const Lesson: React.FC = () => {
             <Guesser
               randomKanas={preparedKanas.slice(0, 4)}
               kanaToGuess={currentKana}
+              handleKanaChoice={handleKanaChoice}
             />
           )}
       </>
       )}
-
+      <Modal
+        isOpen={modalIsOpen}
+        contentLabel="Example Modal"
+      >
+        <button type="button" onClick={() => setIsOpen(false)}>close</button>
+        {correctKana && (
+          <div>
+            your anwser was
+            {' '}
+            {isAnswerCorrect ? 'correct' : 'wrong'}
+            :
+            {' '}
+            {correctKana.japName}
+            {' '}
+            (
+            {correctKana.romName}
+            )
+          </div>
+        )}
+      </Modal>
     </div>
   );
 };
