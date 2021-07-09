e8ccf645e12e557b1959076e084c2fa0cfaf8ac2
alpinagyok <aleks.kozaev@gmail.com>
2021-07-09 15:30:43 +0300

    added linter for functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  ignorePatterns: ['/lib/**/*'],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+    quotes: ['error', 'single'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'array-element-newline': [
+      'error',
+      'consistent',
+    ],
+    'array-bracket-newline': [
+      'error',
+      {
+        multiline: true,
+      },
+    ],
+  },
+};
